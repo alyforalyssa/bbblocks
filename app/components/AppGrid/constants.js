b24bf628182cb54af483d6a8de388a18d3083b4c
Channy <alyssay1226@gmail.com>
2020-05-22 08:28:54 -0700

    feat: appgrid constants

---
+++ app/components/AppGrid/constants.js
@@ -0,0 +1,32 @@
+const allBlocks = [
+  {
+    id: 'paragraph',
+    name: 'Text',
+    image: '',
+    desciption: 'Cute, friendly line of text.',
+    defaultProps: {
+      row: 1,
+      col: 2,
+      text: 'Hello!',
+      style: {
+        color: 'blue',
+      },
+    },
+  },
+  {
+    id: 'header1',
+    name: 'Title',
+    image: '',
+    description: 'Big, bold header.',
+    defaultProps: {
+      row: 1,
+      col: 2,
+      text: 'Title!',
+      style: {
+        color: 'blue',
+      },
+    },
+  },
+];
+
+export { allBlocks };
